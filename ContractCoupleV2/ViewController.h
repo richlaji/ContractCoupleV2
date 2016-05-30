3ab269d5e93cc74cfe54136a8d39359c2e89383f
richlaji <494846236@qq.com>
2016-05-30 14:56:38 +0800

    Initial Commit

---
+++ ContractCoupleV2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContractCoupleV2
+//
+//  Created by 张泽昕 on 16/5/30.
+//  Copyright © 2016年 张泽昕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ContractCoupleV2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContractCoupleV2
+//
+//  Created by 张泽昕 on 16/5/30.
+//  Copyright © 2016年 张泽昕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
